f1bc64a4dbcda69e61977ff0bf095e952ce31702
VictorCyuvi20 <victoramancio2020@gmail.com>
2025-06-02 23:12:11 -0300

    ROTAS PARA CADA PAGINA FO HTML

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, session
+
+
+
+app=Flask(__name__)
+
+app.secret_key ="zcggamestore"
+
+
+# ROTAS
+
+@app.route("/")
+def pagina_inicial():
+    return render_template("Pagina_inicial.html")
+
+@app.route("/login")
+def pagina_inicial():
+    return render_template("Pagina_logar.html")
+
+@app.route("/cadastro")
+def pagina_inicial():
+    return render_template("Pagina_cadastrar.html")
+
+@app.route("/carrinho")
+def pagina_inicial():
+    return render_template("Pagina_carrinho.html")
+
+@app.route("/apresentacao")
+def pagina_inicial():
+    return render_template("Pagina_apresentacao.html")
+
+@app.route("/perfil-usuario")
+def pagina_inicial():
+    return render_template("Pagina_perfil-usuario.html")
+
+@app.route("/categoria-jogos")
+def pagina_inicial():
+    return render_template("Pagina_categoria-jogos.html")
+
+@app.route("/comprar-produto")
+def pagina_inicial():
+    return render_template("Pagina_comprar-produto.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=8080)
+
+

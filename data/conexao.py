935d306a9f780248c0110661aaad33880c8bbfa8
VictoriaAlvesR <vitoriaalves.ajax@gmail.com>
2025-06-09 14:27:44 -0300

    add: (adrian) conexão com o banco de dados AIVEN

--- data/conexao.py
+++ data/conexao.py
@@ -3,16 +3,16 @@ import mysql.connector
 class Conexao:
 
     # CONEXAO LOCAL (INTERNO)
-    def conexao():
-        mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="game_store"
-        )
-        return mydb
+    # def conexao():
+    #     mydb = mysql.connector.connect(
+    #     host="localhost",
+    #     user="root",
+    #     password="root",
+    #     database="game_store"
+    #     )
+    #     return mydb
     
-    # CONEXAO AINVEN (EXTERNO)
+    # CONEXAO AINVEN (EXTERNO) VICTOR
     # def conexao():
     #     mydb = mysql.connector.connect(
     #     host="bd-zcgstore-bancodedados-zcgstore-v.c.aivencloud.com",
@@ -22,3 +22,14 @@ class Conexao:
     #     database="game_store"
     #     )
     #     return mydb
+    
+    #conexão aiven adrian
+    def conexao():
+        mydb = mysql.connector.connect(
+            host="localhost",
+            port="25781",
+            user="avnadmin",
+            password="AVNS_jgx-3UO-5H_xZRimX4d",
+            database="game_store"
+        )
+        return mydb

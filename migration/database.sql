cbe6ebacc6f2ee483579f21061648bcdd632ae15
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-06-09 17:24:26 -0300

    add: mostrar a imagem nome e descrição do jogo na pagina de compra do produto

--- migration/database.sql
+++ migration/database.sql
@@ -44,32 +44,31 @@ CREATE TABLE tb_hisorico(
     preco_porduto float
 );
 
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("elden ring", 299.99, "um mundo tomado pelo mal e vc é o unico que pode parar isso Setornando o Elden Ring");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("dark souls", 299.99, "derrote chefes e se torne o dark souls");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("dark souls II", 299.99, "derrote chefes e se torne o dark souls II");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("dark souls III", 299.99, "derrote chefes e se torne o dark souls III");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("GTA VI", 299.99, "explore se divirta e faça o que bem entender");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("Call of Duty MW III", 299.99, "defenda todos de criminosos terroristas");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("Assassin's Creed Rogue", 299.99, "lute para proteger continentes sendo um assassino de uma irmandade que não vê o desastre que causa");
-insert into tb_jogo(nome_jogo, preco, descricao_jogo) values("Bloodborne", 299.99, "um caçador chega a Yharnam, enfrenta monstros e deuses para escapar de um pesadelo.");
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('elden ring', 299.99, 'um mundo tomado pelo mal e vc é o unico que pode parar isso Setornando o Elden Ring');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('dark souls', 299.99, 'derrote chefes e se torne o dark souls');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('dark souls II', 299.99, 'derrote chefes e se torne o dark souls II');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('dark souls III', 299.99, 'derrote chefes e se torne o dark souls III');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('GTA VI', 299.99, 'explore se divirta e faça o que bem entender');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('Call of Duty MW III', 299.99, 'defenda todos de criminosos terroristas');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('Assassin s Creed Rogue', 299.99, 'lute para proteger continentes sendo um assassino de uma irmandade que não vê o desastre que causa');
+insert into tb_jogo(nome_jogo, preco, descricao_jogo) values('Bloodborne', 299.99, 'um caçador chega a Yharnam, enfrenta monstros e deuses para escapar de um pesadelo.');
 
-insert into foto_produtos(url) values("./static/img/eldenring.png");
-insert into foto_produtos(url) values("./static/img/dark souls.jpg");
-insert into foto_produtos(url) values("./static/img/dark souls 2.jpg");
-insert into foto_produtos(url) values("./static/img/dark souls 3.jpg");
-insert into foto_produtos(url) values("./static/img/GTA VI.jpg");
-insert into foto_produtos(url) values("./static/img/mw3.jpg");
-insert into foto_produtos(url) values("./static/img/rogue.jpg");
-insert into foto_produtos(url) values("./static/img/bloodborne.jpg");
+insert into foto_produtos(url) values('../static/img/eldenring.png');
+insert into foto_produtos(url) values('../static/img/dark souls.jpg');
+insert into foto_produtos(url) values('../static/img/dark souls 2.jpg');
+insert into foto_produtos(url) values('../static/img/dark souls 3.jpg');
+insert into foto_produtos(url) values('../static/img/GTA VI.jpg');
+insert into foto_produtos(url) values('../static/img/mw3.jpg');
+insert into foto_produtos(url) values('../static/img/rogue.jpg');
+insert into foto_produtos(url) values('../static/img/bloodborne.jpg');
 
-insert into tb_categoria(categoria) values("RGP e AÇÃO");
-insert into tb_categoria(categoria) values("RGP e AÇÃO");
-insert into tb_categoria(categoria) values("RGP e AÇÃO");
-insert into tb_categoria(categoria) values("RGP e AÇÃO");
-insert into tb_categoria(categoria) values("AÇÃO e AVENTURA");
-insert into tb_categoria(categoria) values("FPS");
-insert into tb_categoria(categoria) values("AÇÃO e AVENTURA");
-insert into tb_categoria(categoria) values("RGP e AÇÃO");
+insert into tb_categoria(categoria) values('RGP e AÇÃO');
+insert into tb_categoria(categoria) values('RGP e AÇÃO');
+insert into tb_categoria(categoria) values('RGP e AÇÃO');
+insert into tb_categoria(categoria) values('RGP e AÇÃO');
+insert into tb_categoria(categoria) values('AÇÃO e AVENTURA');
+insert into tb_categoria(categoria) values('FPS');
+insert into tb_categoria(categoria) values('AÇÃO e AVENTURA');
+insert into tb_categoria(categoria) values('RGP e AÇÃO');
 
 select * from tb_jogo inner join foto_produtos ON tb_jogo.cod_jogo = foto_produtos.cod_foto inner join tb_categoria ON tb_jogo.cod_jogo = tb_categoria.cod_categoria;
-
--- model/control.py
+++ model/control.py
@@ -23,7 +23,7 @@ def pega_jogos_destaque():
         conexao = CX.Conexao.conexao()
         cursor = conexao.cursor(dictionary = True)
 
-        sql = """select * from tb_jogo inner join foto_produtos ON tb_jogo.cod_jogo = foto_produtos.cod_foto inner join tb_categoria ON tb_jogo.cod_jogo = tb_categoria.cod_categoria WHERE nome_jogo = "GTA VI" or nome_jogo = "elden ring" or nome_jogo = "Assassin's Creed Rogue";"""
+        sql = """select * from tb_jogo inner join foto_produtos ON tb_jogo.cod_jogo = foto_produtos.cod_foto inner join tb_categoria ON tb_jogo.cod_jogo = tb_categoria.cod_categoria WHERE nome_jogo = 'GTA VI' or nome_jogo = 'elden ring' or nome_jogo = 'Assassin s Creed Rogue';"""
 
         cursor.execute(sql, )
 
@@ -39,7 +39,7 @@ def pega_jogos_destaque2():
         conexao = CX.Conexao.conexao()
         cursor = conexao.cursor(dictionary = True)
 
-        sql = """select * from tb_jogo inner join foto_produtos ON tb_jogo.cod_jogo = foto_produtos.cod_foto inner join tb_categoria ON tb_jogo.cod_jogo = tb_categoria.cod_categoria WHERE nome_jogo = "dark souls" or nome_jogo = "dark souls II" or nome_jogo = "dark souls III";"""
+        sql = """select * from tb_jogo inner join foto_produtos ON tb_jogo.cod_jogo = foto_produtos.cod_foto inner join tb_categoria ON tb_jogo.cod_jogo = tb_categoria.cod_categoria WHERE nome_jogo = 'dark souls' or nome_jogo = 'dark souls II' or nome_jogo = 'dark souls III';"""
 
         cursor.execute(sql, )
 
@@ -70,3 +70,22 @@ def busca_jogos_por_nome(termo):
     conexao.close()
 
     return resultados
+
+
+def comprar_produto(codigo):
+
+        conexao = CX.Conexao.conexao()
+        cursor = conexao.cursor(dictionary=True)
+
+        valor = (codigo,)
+
+        sql = """select * from tb_jogo inner join foto_produtos ON tb_jogo.cod_jogo = foto_produtos.cod_foto inner join tb_categoria ON tb_jogo.cod_jogo = tb_categoria.cod_categoria WHERE cod_jogo = %s;"""
+
+        cursor.execute(sql, valor)
+
+        infos = cursor.fetchone()
+
+        cursor.close()
+        conexao.close()
+
+        return infos
